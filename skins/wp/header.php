926d6be170534c360d882aa9c678f77df019a3bf
vollenhove <vollenhove@gmail.com>
2014-12-27 20:34:50 +0100

    Create header.php

---
+++ skins/wp/header.php
@@ -0,0 +1,8 @@
+<?php require('../wp-blog-header.php'); ?>
+<?php get_header(); ?><? wp_head(); ?>
+
+		<div id="primary" class="content-area">
+			<div id="content" class="site-content" role="main">
+
+			
+
