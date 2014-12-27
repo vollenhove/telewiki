960fad45151a0f1007a4d5ffea14077b1b48d6f1
vollenhove <vollenhove@gmail.com>
2014-12-27 20:33:28 +0100

    Create wp.i18n.php

---
+++ skins/wp/wp.i18n.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Internationalization file for skin wp.
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+$messages = array();
+
+/** English
+ * @author Garrick Van Buren
+ */
+$messages['en'] = array(
+	'skinname-wp' => 'wp',
+	'wp-desc' => 'Provides a skin that focuses on putting your content in the wp',
+	'wp-browsermsg' => 'may not look as expected in this version of Internet Explorer. We recommend you upgrade to a newer version of Internet Explorer or switch to a browser like Firefox or Chrome.',
+	'wp-menutitle' => 'Menu'
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'skinname-wp' => 'wp',
+	'wp-desc' => 'Stellt eine Benutzeroberfläche bereit, die die Inhalte des Wikis betont',
+	'wp-browsermsg' => 'sieht in dieser Version des Internet Explorers nicht wie gewünscht aus. Es wird ein Upgrade des Internet Explorers oder der Umstieg auf einen Browser wie Firefox oder Chrome empfohlen.',
+	'wp-menutitle' => 'Menü'
+);
+
+/** Spanish (español)
+ * @author Manuel Rubio 
+ */
+$messages['es'] = array(
+	'skinname-wp' => 'wp',
+	'wp-desc' => 'Provee una plantilla que se centra en resaltar en primer plano el contenido.',
+	'wp-browsermsg' => 'puede verse diferente a lo esperado en esta versión de Internet Explorer le recomendamos que se actualice a una versión más reciente, o cambie a un navegador como Firefox o Chrome.',
+	'wp-menutitle' => 'Menu'
+);
+
+/** Dutch (nederlands)
+ * @author Marijke Stuivenberg
+ */
+$messages['nl'] = array(
+		'skinname-wp' => 'wp',
+		'wp-desc' => 'Biedt een uiterlijk dat er voor zorgt dat de inhoud op de voorgrond wordt weergegeven',
+		'wp-browsermsg' => 'wordt mogelijk niet goed weergegeven in deze versie van Internet Explorer. Aanbevolen wordt te upgraden naar een nieuwere versie van Internet Explorer of om te schakelen naar een browser zoals Firefox of Chrome.',
+		'wp-menutitle' => 'Menu'
+);
+
+/** Arabic
+ * @author النعيمي باشا
+ */
+$messages['ar'] = array(
+	'skinname-wp' => 'wp',
+	'wp-desc' => 'نوفر مظهر يركز على وضع محتواك في المقدمة',
+	'wp-browsermsg' => 'ربما يظهر بشكل غير مقبول في هذه النسخة من انترنت اكسبلورر. ننصحك بالترقية إلى إصدار أحدث لانترنت اكسبلورر أو التغير إلى متصفح مثل فايرفوكس أو كروم.',
+	'wp-menutitle' => 'قائمة'
+);
+
+/** Russian
+ *  @author HiRolla
+ */
+$messages['ru'] = array(
+	'skinname-wp' => 'wp',
+	'wp-desc' => 'Оформление, которое выводит на передний план именно содержимое страниц, минимизируя интерфейс сайта',
+	'wp-browsermsg' => 'Возможны непредвиденные изменения вида в Internet Explorer. Рекомендуем обновить Internet Explorer или пользоваться такими браузерами, как Firefox или Chrome.',
+	'wp-menutitle' => 'Меню'
+);
+
